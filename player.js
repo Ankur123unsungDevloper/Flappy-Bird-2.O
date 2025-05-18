cda7c854e99d854083a4e91937a58e2823149d86
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-05-18 10:37:41 +0530

    new file:   index.html 	new file:   main.js 	new file:   player.js 	new file:   style.css

---
+++ player.js
@@ -0,0 +1,21 @@
+class Player {
+  constructor (game) {
+    this.game = game;
+    this.x = 50;
+    this.y = 60;
+    this.spriteWidth = 200;
+    this.spriteHeight = 200;
+    this.width;
+    this.height;
+  }
+  draw() {
+    this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  update() {
+    //this.x++;
+  }
+  resize() {
+    this.width = this.spriteWidth * this.game.ratio;
+    this.height = this.spriteHeight * this.game.ratio;
+  }
+}
